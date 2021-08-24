468cf19e0fab1aabc93439d8cfc54daefe2f1110
Pedro Mazo <pedrolmazo@gmail.com>
2021-08-24 15:01:52 -0300

    added mongoose

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const ejsMate = require('ejs-mate');
 const path = require('path');
+const methodOverride = require('method-override');
 const port = 3000;
 const app = express();
+const mongoose = require('mongoose');
 
-app.engine('ejs', ejsMate) 
+app.engine('ejs', ejsMate); 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
+
+mongoose.connect('mongodb://localhost:27017/calendar', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+const db = mongoose.connection; 
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
@@ -14,4 +28,4 @@ app.listen(port, () => {
 
 app.get('/', (req,res) => [
     res.render('home.ejs')
-])
+]) 
